eeabf771ae6ca0d365ba9009e92782b1394e7ced
AjAutomation <AjAutomation09@gmail.com>
2023-03-22 11:51:13 +0530

    My First commit

---
+++ src/test/java/testPackage/SecondTest.java
@@ -0,0 +1,15 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+    @Test
+    public void m3() {
+        System.out.println("m3 executed");
+    }
+    @Test
+    public void m4() {
+        System.out.println("m4 executed");
+    }
+
+}
