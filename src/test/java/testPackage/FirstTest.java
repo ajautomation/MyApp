6a7d923b0be0e4623b109a7131f03e4566a3000b
AjAutomation <AjAutomation09@gmail.com>
2023-03-30 22:44:28 +0530

    Revert "Updated FirstTest.java"
    This reverts commit 15b583c89eb92c41e2976b4ba9a04ce879261955.

--- src/test/java/testPackage/FirstTest.java
+++ src/test/java/testPackage/FirstTest.java
@@ -8,8 +8,6 @@ public class FirstTest {
         System.out.println("m1 executed");
         System.out.println("New commit");
         //testing new chnage in github
-		//Line change1
-		//Line change 2
     }
     @Test
     public void m2() {
