15b583c89eb92c41e2976b4ba9a04ce879261955
AjAutomation <AjAutomation09@gmail.com>
2023-03-30 22:42:00 +0530

    Updated FirstTest.java

--- src/test/java/testPackage/FirstTest.java
+++ src/test/java/testPackage/FirstTest.java
@@ -8,6 +8,8 @@ public class FirstTest {
         System.out.println("m1 executed");
         System.out.println("New commit");
         //testing new chnage in github
+		//Line change1
+		//Line change 2
     }
     @Test
     public void m2() {
